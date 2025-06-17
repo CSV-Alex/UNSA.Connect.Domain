5a6430295de7ffd806420b1fe3f18bd2bc02121b
CSV-Alex <acanapatana@unsa.edu.pe>
2025-06-16 20:55:43 -0500

    Fixed DB version

--- Model/Repository/SQLServer/NotificationRepositoryImpl.cs
+++ Model/Repository/SQLServer/NotificationRepositoryImpl.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Repository.MySQL{
+namespace Repository.SQLServer{
     public class NotificationRepositoryImpl : INotificationRepository {
 
         public NotificationRepositoryImpl() {
